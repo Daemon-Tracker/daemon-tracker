82db5a294527b0f56ef38daeb3415f0c6f09b248
andi-frame <andifarhan1094@gmail.com>
2024-08-22 00:22:54 +0700

    Fix: Error because invalid auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -9,12 +9,12 @@ import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
-import {
-  accounts,
-  sessions,
-  users,
-  verificationTokens,
-} from "~/server/db/schema";
+// import {
+//   accounts,
+//   sessions,
+//   users,
+//   verificationTokens,
+// } from "~/server/db/schema";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -42,42 +42,42 @@ declare module "next-auth" {
  *
  * @see https://next-auth.js.org/configuration/options
  */
-export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }) as Adapter,
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-};
+// export const authOptions: NextAuthOptions = {
+//   callbacks: {
+//     session: ({ session, user }) => ({
+//       ...session,
+//       user: {
+//         ...session.user,
+//         id: user.id,
+//       },
+//     }),
+//   },
+//   adapter: DrizzleAdapter(db, {
+//     usersTable: users,
+//     accountsTable: accounts,
+//     sessionsTable: sessions,
+//     verificationTokensTable: verificationTokens,
+//   }) as Adapter,
+//   providers: [
+//     DiscordProvider({
+//       clientId: env.DISCORD_CLIENT_ID,
+      // clientSecret: env.DISCORD_CLIENT_SECRET,
+//     }),
+//     /**
+//      * ...add more providers here.
+//      *
+//      * Most other providers require a bit more work than the Discord provider. For example, the
+//      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+//      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+//      *
+//      * @see https://next-auth.js.org/providers/github
+//      */
+//   ],
+// };
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => getServerSession(authOptions);
+// export const getServerAuthSession = () => getServerSession(authOptions);
