81b9a54610fdc37f4f1a9d95c9c474d6f1ec4699
Tuyulll <19623185@std.stei.itb.ac.id>
2024-08-23 01:42:45 +0700

    feat: session restriction

--- src/app/home/admin/page.tsx
+++ src/app/home/admin/page.tsx
@@ -3,8 +3,17 @@ import SearchBar from "~/app/_components/searchBar";
 import SudoListAdmin from "~/app/_components/sudoListAdmin";
 import Navbar from "~/app/_components/navbar";
 import { SudoContextProvider } from "~/app/_context/sudoContext";
+import { getServerAuthSession } from "~/server/auth";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerAuthSession();
+
+  if (!session || session.user.role !== "admin") {
+    redirect("/");
+    return null;
+  }
+
   return (
     <div className="max-w-screen flex flex-col items-center justify-center">
       <Navbar />
