195dbae71e0a9d9c545d4d3252aa1589e17d271e
andi-frame <andifarhan1094@gmail.com>
2024-08-23 17:51:27 +0700

    Feat: Update login-page

--- src/app/_components/signIn.tsx
+++ src/app/_components/signIn.tsx
@@ -21,11 +21,11 @@ export default function SignIn() {
         <div>
             <button
                 onClick={() => signIn("google")}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+                className="rounded-full bg-gradient-to-r from-[#000000] to-[#434343] px-20 py-3 font-semibold no-underline transition hover:bg-white/20 text-white"
                 >
                 Sign in
             </button>
-            {error && <p className="text-red-600">{error}</p>}
+            {error && <p className="text-red-600 text-center pt-3 font-bold">{error}</p>}
         </div>
     )
 }
